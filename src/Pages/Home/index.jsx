9ca9d8468a94cf4ce5f61f9fe5048a4652667996
Francisco Crespo <fmfcrespo@gmail.com>
2023-06-05 18:06:31 +0100

    Homepage Carousel

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,53 @@
 function HomePage() {
 
   return (
+  
+    <div> 
 
-    <div>
-        <h1>Homepage</h1>
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="public/images/Carousel1.jpg" class="d-block w-100"></img>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
     </div>
+    <div class="carousel-item">
+      <img src="public/images/motorcycles.jpg" class="d-block w-100"></img>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Want to check out more motorcycles ?!.</p>
+        <a href="/motorcycles">Check all the brands here!!</a>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="public/images/marketplace.jpg" class="d-block w-100"></img>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p> Looking for that perfect exaust pipe? Or some warmer gloves?</p>
+        <a href="/marketplace">Check out our Marketplace</a>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
-  )
+</div>
+
+)
 }
 
 export default HomePage
